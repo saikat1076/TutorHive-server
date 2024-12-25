f10aad28f84ff093e67ff6d018e489ac30e50b88
saikat1076 <chakrabortymoni335@gmail.com>
2024-12-26 00:24:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rt6v2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const tutorCollection = client.db('tutorhive').collection('tutors');
+        const categoryCollection = client.db('tutorhive').collection('category');
+        app.post('/tutors', async (req, res) => {
+            const newTutor = req.body;
+            console.log(newTutor);
+            const result = await tutorCollection.insertOne(newTutor);
+            res.send(result);
+
+        })
+
+        app.get('/tutors', async (req, res) => {
+            const category = req.query.category;
+
+            if (category) {
+                const tutors = await tutorCollection.find({ category }).toArray();
+                res.send(tutors);
+            } else {
+                const tutors = await tutorCollection.find().toArray();
+                res.send(tutors);
+            }
+        });
+
+        app.get('/tutors/:id', async (req, res) => {
+            const id = req.params.id;
+            try {
+              const query = { _id: new ObjectId(id) };
+              const result = await tutorCollection.findOne(query);
+              res.send(result);
+            } catch (error) {
+              console.error(error);
+              res.status(500).send({ message: 'Invalid ID or Database Error' });
+              }
+            });
+
+        app.get('/tutors/email/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            try {
+              const result = await tutorCollection.find(query).toArray();
+              res.send(result);
+            } catch (error) {
+              console.error(error);
+              res.status(500).send({ message: 'Error fetching data' });
+            }
+          });
+
+
+          app.delete('/tutors/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await tutorCollection.deleteOne(query);
+            res.send(result);
+          })
+        
+
+        app.get('/category', async (req, res) => {
+            const cursor = categoryCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close(); 
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('TutorHive is running')
+})
+
+app.listen(port, () => {
+    console.log(`TutorHive is running on port: ${port}`);
+
+})
